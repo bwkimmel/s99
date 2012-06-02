27a899e7340f11c4b91c94daa01e798840cb5995
Brad Kimmel <intrepidca@gmail.com>
2012-06-01 23:43:22 -0400

    P47

--- src/ca/eandb/s99/S99Logic.scala
+++ src/ca/eandb/s99/S99Logic.scala
@@ -8,9 +8,25 @@ package ca.eandb.s99
  * To change this template use File | Settings | File Templates.
  */
 
+case class S99Logic(a: Boolean) {
+
+  /** P47 */
+  def nand(b: Boolean) = !(a && b)
+  def and(b: Boolean) = a && b
+  def or(b: Boolean) = a || b
+  def nor(b: Boolean) = !(a || b)
+  def xor(b: Boolean) = a ^ b
+  def impl(b: Boolean) = !a || b
+  def equ(b: Boolean) = a == b
+
+}
+
 object S99Logic {
 
   import Util._
+  
+  implicit def boolean2S99Logic(a: Boolean) = S99Logic(a)
+  implicit def s99Logic2Boolean(logic: S99Logic) = logic.a
 
   def nand(a: Boolean, b: Boolean) = !(a && b)
   def and(a: Boolean, b: Boolean) = a && b
@@ -19,6 +35,7 @@ object S99Logic {
   def xor(a: Boolean, b: Boolean) = a ^ b
   def impl(a: Boolean, b: Boolean) = !a || b
   def equ(a: Boolean, b: Boolean) = a == b
+  def not(a: Boolean) = !a
 
   /** P46 */
   def table2(f: (Boolean, Boolean) => Boolean) = {
