6557bb3eb6611a74b7a0cdc782783ba314054e6e
Brad Kimmel <intrepidca@gmail.com>
2012-06-01 23:11:07 -0400

    Initial commit of S99Logic.

---
+++ src/ca/eandb/s99/S99Logic.scala
@@ -0,0 +1,13 @@
+package ca.eandb.s99
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brad
+ * Date: 6/1/12
+ * Time: 11:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class S99Logic {
+
+}
