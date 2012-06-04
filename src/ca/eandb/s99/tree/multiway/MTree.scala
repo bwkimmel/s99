736c5ba8675889af6232974fc17522ecf276328a
Brad Kimmel <intrepidca@gmail.com>
2012-06-04 00:56:07 -0400

    P70C

--- src/ca/eandb/s99/tree/multiway/MTree.scala
+++ src/ca/eandb/s99/tree/multiway/MTree.scala
@@ -10,11 +10,14 @@ package tree.multiway
  */
 
 case class MTree[+T](value: T, children: List[MTree[T]]) {
-  def this(value: T) = this(value, List())
+
   override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+
+  /** P70C */
+  def nodeCount: Int = 1 + children.map(_.nodeCount).sum
+
 }
 
 object MTree {
-  def apply[T](value: T) = new MTree(value, List())
-  def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
+  def apply[T](value: T) = new MTree(value, Nil)
 }
